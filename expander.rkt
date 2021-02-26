f3b6aed335711fc295a494508b16bfcd3c1928c7
Joel Dueck <joel@jdueck.net>
2021-02-26 08:31:03 -0600

    Move defines to top of lambda (not module)

--- expander.rkt
+++ expander.rkt
@@ -10,34 +10,38 @@
 (define (strip-leading-newlines lst)
   (dropf lst (λ (ln) (member ln (list "\n" "")))))
 
-;; Split requires and provides etc. out of a list of expressions
-(define-for-syntax (toplevel-forms-splitter lst)
+;; Split top-level stuff (requires, provides etc.) and defines out of a list of expressions
+(define-for-syntax (forms-splitter lst)
   (let loop ([body lst]
              [toplevelstuff '()]
+             [defines '()]
              [normalstuff '()])
     (syntax-case body ()
-      [() (list (reverse toplevelstuff) (reverse normalstuff))]
+      [() (list (reverse toplevelstuff) (reverse defines) (reverse normalstuff))]
       [((id rest ...) . body2)
        (and (identifier? #'id)
             (ormap (lambda (kw) (free-identifier=? #'id kw))
                    (syntax->list #'(require
                                      provide
-                                     define
-                                     define-values
                                      define-syntaxes
                                      begin-for-syntax
                                      module
                                      module*
                                      #%require
                                      #%provide))))
-       (loop #'body2 (cons #'(id rest ...) toplevelstuff) normalstuff)]
+       (loop #'body2 (cons #'(id rest ...) toplevelstuff) defines normalstuff)]
+      [((id rest ...) . body2)
+       (and (identifier? #'id)
+            (ormap (lambda (kw) (free-identifier=? #'id kw))
+                   (syntax->list #'(define define-values))))
+       (loop #'body2 toplevelstuff (cons #'(id rest ...) defines) normalstuff)]
       [(body1 . body2)
-       (loop #'body2 toplevelstuff (cons #'body1 normalstuff))])))
+       (loop #'body2 toplevelstuff defines (cons #'body1 normalstuff))])))
 
 (define-syntax (beeswax-module-begin stx)
   (syntax-case stx ()
     [(_ . EXPRS)
-     (with-syntax ([((TOPLEVEL ...) (BODY ...)) (toplevel-forms-splitter #'EXPRS)]
+     (with-syntax ([((TOPLEVEL ...) (DEFINES ...) (BODY ...)) (forms-splitter #'EXPRS)]
                    [REQUIRES (datum->syntax #'EXPRS '(require pollen/template pollen/pagetree))]
                    [RENDER (datum->syntax #'EXPRS 'render)]
                    [DOC (datum->syntax #'EXPRS 'doc)]
@@ -49,6 +53,7 @@
           TOPLEVEL ...
           (define/contract (RENDER DOC METAS HERE)
             (-> any/c hash? path-string? bytes?)
+            DEFINES ...
             (concat+write/bytes HERE (list . (BODY ...))))))]))
 
 (define (->string->bytes x)
