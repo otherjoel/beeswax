fe675451729b11cb2ff485a44bfa0b567f21eef4
Joel Dueck <joel@jdueck.net>
2021-05-29 16:01:54 -0500

    Skip parameterize in Pollen renders

--- for-pollen.rkt
+++ for-pollen.rkt
@@ -13,11 +13,10 @@
 (define (external-renderer source-path orig-template-path output-path)
   (case (path-get-extension source-path)
     [(#".pm" #".pmd")
-     (parameterize ([current-poly-target (string->symbol (get-ext output-path))])
-       (match-define-values
-        ((cons render-result _) _ ms _)
-        (time-apply render (list source-path output-path)))
-       (log-info "beeswax rendered /~a (~ams)"
-                 (find-relative-path (current-project-root) output-path) ms)
-     render-result)]
+     (match-define-values
+      ((cons render-result _) _ ms _)
+      (time-apply render (list source-path output-path)))
+     (log-info "beeswax rendered /~a (~ams)"
+               (find-relative-path (current-project-root) output-path) ms)
+     render-result]
     [else (pollen:render source-path orig-template-path)]))
