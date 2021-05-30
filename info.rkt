707ca68d51c398d23e9efe6f20a3fc15cb295100
Joel Dueck <joel@jdueck.net>
2021-05-30 10:04:35 -0500

    Fix dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,11 @@
 #lang info
 (define collection "beeswax")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("at-exp-lib"
+               "pollen"
+               "sugar"
+               "base"))
+(define build-deps '("scribble-doc"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/beeswax.scrbl" (multi-page))))
 (define raco-commands '(("beeswax" (submod beeswax/private/command raco) "issue Beeswax command" #f)))
 (define pkg-desc "Description Here")
