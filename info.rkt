23278970b51065d5ba0ca4f78db9ecab2a13f747
Joel Dueck <joel@jdueck.net>
2021-05-30 23:01:48 -0500

    Put something there

--- info.rkt
+++ info.rkt
@@ -8,6 +8,6 @@
                      "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/beeswax.scrbl" (multi-page))))
 (define raco-commands '(("beeswax" (submod beeswax/private/command raco) "issue Beeswax command" #f)))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "A Pollen-friendly template language")
+(define version "0.1")
 (define pkg-authors '(joel))
